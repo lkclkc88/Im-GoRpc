e172632ec8770fe7ade3db812379798085575ede
liaokucheng <lkclkc88@sina.com>
2018-07-26 15:21:49 +0800

    添加buildSeq方法

--- src/im/go/rpc/imPack.go
+++ src/im/go/rpc/imPack.go
@@ -3,9 +3,22 @@ package rpc
 import (
 	"encoding/binary"
 	"errors"
+	"sync/atomic"
+	"time"
 )
 
 const MAX_SIZE = 10214 * 1024
+
+var seq_index uint32 = 0
+var seq_tmp uint32 = (1 << 27) - 1
+
+//构建seq，seq用于请求唯一标示，并且在同一个socket中使用，因此只需要保证几秒内数据不重复即可,因此保证1分钟内数据不重复
+func BuildSeq() uint32 {
+	index := atomic.AddUint32(&seq_index, 1)
+	now := time.Now()
+	result := uint32((now.Second() << 26)) + (index & seq_tmp)
+	return uint32(result)
+}
 
 /*
 协议头
